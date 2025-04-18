4cf550597dd484c411b4b6f4aee8efb636150ab8
jacan <jckandersen@gmail.com>
2025-04-18 12:43:40 +0200

    Like a tuple return, this is standard Go error handling

--- src/tutorial-2/hello/hello.go
+++ src/tutorial-2/hello/hello.go
@@ -2,11 +2,22 @@ package main
 
 import (
   "fmt"
+  "log"
 
   "example.com/greetings"
 )
 
 func main() {
-  message := greetings.Hello("Paul")
+  // adjust the loggers output
+  log.SetPrefix("greetings: ")
+  // Controlling standard log output like date, line number, source file
+  log.SetFlags(0)
+
+  message, err := greetings.Hello("")
+  if(err != nil) {
+    // Fatal causes process exit!
+    log.Fatal(err)
+  }
+
   fmt.Println(message)
 }
