cab19e1fde29556522208a7a5442cbb4e7e0b46b
jacan <jckandersen@gmail.com>
2025-04-18 13:37:17 +0200

    Introduce a bug to show go test - test failure

--- src/tutorial-2/greetings/greetings.go
+++ src/tutorial-2/greetings/greetings.go
@@ -12,7 +12,10 @@ func Hello(name string) (string, error) {
     return "", errors.New("empty name")
   }
 
-  message := fmt.Sprintf(randomFormat(), name)
+  // message := fmt.Sprintf(randomFormat(), name)
+
+  message := fmt.Sprint(randomFormat())
+
   return message, nil
 }
 
